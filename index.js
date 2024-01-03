8fbc26912c14fe842680a04a7bf0cb74f96c5411
UR Raju <riadhasanraju>
2024-01-04 03:34:49 +0600

    tonight last push

--- index.js
+++ index.js
@@ -51,6 +51,7 @@ app.use(cors({
     try {
         // collection create 
         const  productsCollection = client.db('emDB').collection('products')
+        const  userproductCollection = client.db('emDB').collection('userProduct')
     
     // Token post 
     app.post('/jwt', async(req,res) => {
@@ -77,17 +78,31 @@ app.use(cors({
         const result = await productsCollection.find().toArray()
         res.send(result)
     })
-    app.get('/products', async(req,res) => {
+    app.get('/products', logger,verifyToken, async(req,res) => {
         const result = await productsCollection.find().toArray()
         res.send(result)
     })
-    app.get('/products/:id', async(req,res) => {
+    app.get('/products/:id',  async(req,res) => {
         const id = req.params.id
         const query = {_id : new ObjectId(id)}
         const result = await productsCollection.findOne(query)
         res.send(result)
      })
      
+      // user product buy api 
+      app.post('/userProduct', async (req,res) => {
+        const query = req.body
+        const result = await userproductCollection.insertOne(query)
+        res.send(result)
+      })
+     app.get("/userProduct", async (req, res) => {
+        const query = req.query?.email
+        const filter = {userEmail : query}
+        console.log('email',query);
+        const result = await userproductCollection.find(filter).toArray();
+        res.send(result);
+      });
+
       await client.db("admin").command({ ping: 1 });
       console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
